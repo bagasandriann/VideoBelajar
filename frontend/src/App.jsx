232dcf40e0d5dfc77b6318ab2d7ec741fdc25a5f
bagasandriann <bagasandriann@gmail.com>
2026-06-13 16:11:39 +0700

    Adding Browser Route
    adding broser route so the url can be route based on pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
   return (
-    <main className="min-h-screen bg-[#f7f3ef] flex items-center justify-center px-6">
-      <section className="text-center">
-        <h1 className="text-4xl font-bold text-[#f64920]">VideoBelajar</h1>
-        <p className="mt-3 text-lg text-slate-700">
-          React dan Tailwind sudah berhasil jalan.
-        </p>
-      </section>
-    </main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-[#f7f3ef] flex items-center justify-center px-6">
+      <section className="text-center">
+        <h1 className="text-4xl font-bold text-[#f64920]">VideoBelajar</h1>
+        <p className="mt-3 text-lg text-slate-700">
+          Ini Halaman Login.
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default App
